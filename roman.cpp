2e4d687dcf4eac9136ae1e568f2d6870ded2f2ce
Mahiragrawal <mihiragrawal022@gmail.com>
2020-05-20 13:50:33 +0530

    MADE ROMAN NUMBER PROJECT

---
+++ roman.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#define cout std::cout
+#define cin std::cin
+#define endl std::endl
+class roman{
+int roman_num = 0;
+char *roman_str = NULL;
+int value(char alpha)
+{
+if(alpha == 'M' || alpha == 'm')
+	return 1000;
+else if(alpha == 'D' || alpha == 'd')
+	return 500;
+else if(alpha == 'C' || alpha == 'c')
+	return 100;
+else if(alpha == 'L' || alpha == 'l')
+	return 50;
+else if(alpha == 'X' || alpha == 'x')
+	return 10;
+else if(alpha == 'V' || alpha == 'v')
+	return 5;
+else if(alpha == 'I' || alpha == 'i')
+	return 1;
+else{
+cout<<"SOMETHING WRONG HAS HAPPENED WITH YOUR INPUT!!!<<endl";
+return;
+}
+}
+void set_roman_num(char *s)
+{
+int *temp_num = NULL;
+int size = strlen(s);
+temp_num = new int[size];
+temp_num[0] = value(s[0]);
+for(int i = 0;i < (size-1);i++)
+{
+if(value(s[i]) < value(s[i+1]))
+	temp_num[i+1] = (-1)*value(s[i+1]);
+else
+	temp_num[i+1] = value(s[i+1]);
+}
+for(int i = 0;i < size;i++)
+	roman_num += temp_num[i];
+delete []temp_num;
+}
+public:
+roman()
+{
+roman_num = 0;
+roman_str = NULL;
+}
+roman(char *temp_str)
+{
+roman_str = temp_str;
+set_roman_num(roman_str);
+}
+void add()
+{
+char name[10];
+cout<<"ENTER THE ROMAN NUMBER: ";
+cin<<name;
+int size = strlen(name);
+roman_str = new char[size+1];
+strcpy(roman_str,name);
+set_roman_num(roman_str);
+cout<<"YOUR NUMBER HAS BEEN ADDED!!!!"<<endl;
+}
+void display()
+{
+cout<<"ROMAN NUMBER: "<<roman_str<<endl;
+cout<<"DECIMAL EQUIVALENT: "<<roman_num<<endl;
+}
+};
+int main()
+{
+roman r("XL");
+r.display();
+}
