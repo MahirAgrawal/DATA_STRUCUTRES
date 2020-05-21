38bae03bddc045a9c9487715a073698cab72cba3
MIHIR <mihiragrawal022@gmail.com>
2020-05-21 18:11:06 +0530

    ESTING IS LEFT

--- array.cpp
+++ array.cpp
@@ -6,7 +6,7 @@ class array{
 X *arr = NULL;
 X *begin = NULL;
 X *end = NULL;
-//bool is_empty = true;
+bool is_empty = true;
 bool is_full = false;
 int max_size = 16;
 int size = 2;
@@ -21,7 +21,7 @@ if(begin == NULL)
 	}
 arr = begin;
 end = begin + s - 1;
-//is_empty = true;
+is_empty = true;
 is_full = false;
 }
 void resize()
@@ -44,19 +44,24 @@ cout<<"CANNOT ADD MORE ELEMENTS!!"<<endl;
 return;
 }	
 arr[arr-begin] = value;
+is_empty = false;
 arr++;
 if(arr > end)
 	resize(); 
 }
 void pop_back()
 {
-if(arr == begin)
+if(is_empty)
 {
 printf("ARRAY IS EMPTY!!!\n");
 return;
 }
 else
+	{
 	arr--;
+	if(arr == begin)
+		is_empty = true;	
+	}
 }
 void display()
 {
@@ -66,14 +71,36 @@ X *ptr = begin;
 	cout<<"ELEMENTS: "<<*ptr<<" ";
 	ptr++;
 	}
+//
+cout<<"ARR"<<arr<<endl;
+cout<<"BEGIN:"<<begin<<endl;
+cout<<"END:"<<end<<endl;
+cout<<"IS_EMPTY"<<is_empty<<endl;
+cout<<"IS_FULL"<<is_full<<endl;
+cout<<"MAX_SIZE:"<<max_size<<endl;
+cout<<"SIZE:"<<size<<endl;
+//
 }
-X at(int index)
-{
-index()
-}
+X& at(int index)
+/*{
+X& temp = begin[index];
+return temp;
+}*/
 };
 int main()
 {
-arra
+array<int> marks;
+marks.push_back(122);
+marks.display();
+marks.push_back(22);
+marks.display();
+marks.push_back(12);	
+marks.display();
+marks.push_back(143);
+marks.display();
+marks.push_back(233);
+marks.display();
+marks.push_back(1111);
+marks.display();
 return 0;
 }
