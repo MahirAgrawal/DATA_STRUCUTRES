29d6cce32fcdc990ef62c2ed2663f31004a4cf14
MIHIR <mihiragrawal022@gmail.com>
2020-05-22 13:49:00 +0530

    ADDED ARRAY CLASS

--- mihir.h
+++ mihir.h
@@ -1,3 +1,8 @@
+#include<iostream>
+#include<conio.h>
+#include<cstdio>
+#include<windows.h>
+#include<stdlib.h>
 #define black 0
 #define blue 1
 #define light_gray 7
@@ -11,6 +16,7 @@
 #define cin std::cin
 #define endl std::endl
 #define fsream std::fstream
+#define cerr std::cerr
 char* allocate(const char *source)
 {
 int s = strlen(source);
@@ -212,3 +218,175 @@ coord.Y = 5;
 goto start;
 }
 }
+//array class
+template<class X>
+class array{
+X *arr = NULL;
+X *begin = NULL;
+X *end = NULL;
+bool is_empty = true;
+bool is_full = false;
+int max_size_a = 16;
+int size_a = 2;
+public:
+array(int s = 2)
+{
+begin =  new X[s];
+if(begin == NULL)
+	{
+	printf("ARRAY CANNOT BE ALLOCATED!!\n");
+	return;
+	}
+arr = begin;
+end = begin + s - 1;
+is_empty = true;
+is_full = false;
+}
+X* begin_p()//return pointer to first element
+{
+return begin;
+}
+X* end_p()//return pointer to end index and not the end element
+{
+return end;
+}
+X* arr_p()
+{
+return arr;//return pointer to current writing memory address
+}
+int size()//returns actual size of array based on filled elements
+{
+int count = 0;
+X *i = begin;
+while(i != arr)
+	{count++;i++;}
+return count;
+}
+int max_size()
+{
+return max_size_a;
+}
+bool resize()
+{
+if((end - begin + 1) == max_size_a)
+	{is_full = true;return false;/*as array is not resized*/}	
+else	 
+{
+X *temp = new X[2*size_a];	
+for(int i = 0;i < size_a;i++)
+	temp[i] = begin[i];
+delete []begin;
+begin = temp;
+arr = begin + size_a;
+size_a *= 2;
+end = begin + size_a - 1;
+return true;//as array is resized
+}
+}
+void push_back(X value)
+{
+bool answer = true;
+if(arr > end)
+	answer = resize();
+if(answer == false)
+{
+cout<<"ARRAY IS USED AT IT'S FULL CAPACATIY!!"<<endl;	
+cout<<"CANNOT ADD MORE ELEMENTS!!"<<endl;
+return;
+}
+begin[arr-begin] = value;
+is_empty = false;
+arr++;
+}
+void pop_back()
+{
+if(is_empty)
+	return;
+else
+{
+arr--;
+if(arr == begin)
+	{
+	is_empty = true;	
+	arr = begin;
+	}
+}
+}
+void pop(X *address)
+{
+if(is_empty)
+	return;
+if(address < begin || address >= arr)
+	{
+	cerr<<"POSITION  IS INVALID!!\n";
+	return;
+	}
+int deletions = arr - address - 1; 
+for(int i = 0;i < deletions;i++)
+	address[i]  = address[i+1];
+arr--;
+if(arr == begin)
+	is_empty = true;
+}
+void display()
+{
+if(is_empty)
+	{
+	cout<<"ARRAY IS EMPTY\n";
+	return;
+	}
+X *ptr = begin; 
+cout<<"ELEMENTS: ";
+while(ptr != arr)
+	{
+	cout<<*ptr<<" ";
+	ptr++;
+	}
+cout<<"\n";
+}
+X at(X *address)
+{
+if(address < begin || address > arr)
+	{
+	cout<<"INVALID ADDRESS!\n";
+	return -1;
+	}
+else{
+return *address;}
+}
+void insert(X *address,X value)
+{
+int address_pos = address - begin;
+if(address < begin || address > end)
+	{
+	cerr<<"POSITION  IS INVALID!!\n";
+	return;
+	}
+bool answer = true;
+if(arr > end)
+	answer = resize();
+if(answer == false)
+{
+cout<<"ARRAY IS USED AT IT'S FULL CAPACATIY!!"<<endl;	
+cout<<"CANNOT ADD MORE ELEMENTS!!"<<endl;
+return;
+}
+else{
+address = begin + address_pos;
+int insertions = arr - address;
+for(int i = insertions;i > 0;i--)
+	address[i]  = address[i-1];
+address[0] = value;
+is_empty = false;
+arr++;}
+}
+void clear()
+{
+arr = begin;
+is_empty = true;
+}
+~array()
+{
+delete []begin;
+}
+};
