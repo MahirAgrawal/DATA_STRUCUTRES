4d7e785819017c217387108a25657dcc2cf6724e
MIHIR <mihiragrawal022@gmail.com>
2020-05-23 09:48:23 +0530

    added pop_

--- linked_list.cpp
+++ linked_list.cpp
@@ -12,6 +12,11 @@ node()
 {
 next = NULL;
 }
+~node()
+{
+next = NULL;
+cout<<"DATA WAS DELETED!!!";
+}
 };
 template<class X>
 class list{
@@ -58,35 +63,53 @@ head = ptr;
 }
 index++;
 }
-};
 void pop_back()
 {
-
+if(head == NULL)
+	return;
+node<X> *ptr = head;
+for(int i = 0;i < (index-1);i++)
+	ptr = ptr -> next;
+node<X> *temp = ptr -> next;
+ptr -> next = NULL;
+delete []temp;
+index--;
 }
+void pop_front()
+{
+if(head == NULL)
+	return;
+node<X>* ptr = head;
+head = head -> next;
+delete []ptr;
+index--;
+}
+int size()
+{
+return index+1;
+}
+void display()
+{
+if(head == NULL)
+	return;
+node<X> *ptr = head;
+cout<<"ELEMENTS: ";
+while(ptr != NULL)
+	{
+	cout<<(ptr -> data)<<" ";
+	ptr = ptr -> next;
+	}
+}
+
 int main()
 {
-void display()
-{
-cout<<"DATA:"<<data<<":"<<(unsigned int)&(this->data)<<endl;
-printf("PTR:%u\n",ptr);
-}
-void display()
-{
-cout<<"HEAD:"<<head<<endl;
-n.display();
-}
-node<char>n;
-n.data = 'c';
-n.ptr = &(n.data);
-n.display();
-list<char>l;
-l.head = 'g';
-l.n.data = 'f';
-l.n.ptr = &(l.n.data);
+list<char> l;
+l.push_back('m');
 l.display();
-node<int> *ptr = new node<int>;
-ptr -> data = 34;
-ptr -> ptr = &(ptr -> data);
-ptr -> display();
+l.push_back('i');
+l.display();
+l.push_front('b');
+l.display();
+cout<<"SIZE:"<<l.size();
 return 0;
 }
